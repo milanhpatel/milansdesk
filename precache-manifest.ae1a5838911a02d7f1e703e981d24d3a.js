53cc63ef7bbf0d21af8f8001f2b208f13c1a7fd0
Milan Patel <milanpatel92@gmail.com>
2019-06-06 20:33:19 -0400

    Updates

---
+++ precache-manifest.ae1a5838911a02d7f1e703e981d24d3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e432a057740a9fba41c69d1239e087",
+    "url": "/milansdesk/index.html"
+  },
+  {
+    "revision": "d6f5b2d6a0e30b4a1611",
+    "url": "/milansdesk/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/milansdesk/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d6f5b2d6a0e30b4a1611",
+    "url": "/milansdesk/static/js/main.957ef234.chunk.js"
+  },
+  {
+    "revision": "d4a445ccfbf967f93a64",
+    "url": "/milansdesk/static/js/runtime~main.796e380c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/milansdesk/static/media/logo.5d5d9eef.svg"
+  }
+]);
